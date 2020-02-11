cb316620235add99c178658a8339bc0f0e8a51f0
SoibamRepo <soibam.ana.singh@hpe.com>
2020-02-11 23:45:41 +0530

    initial commit

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
-module github.com/SoibamRepo/hello
+module github.com/SoibamRepo/terraform-provider
 
 go 1.12
+
+require github.com/google/go-cmp v0.4.0
--- hello.go
+++ hello.go
@@ -2,9 +2,12 @@ package main
 
 import (
       "fmt"
+       "github.com/SoibamRepo/terraform-provider/morestrings"
+       "github.com/google/go-cmp/cmp"
       
 )
 
 func main() {
-	fmt.Println("!oG ,oooooolleH")
+	fmt.Println(morestrings.ReverseRunes("!oG ,oooooolleH"))
+        fmt.Println(cmp.Diff("Hello World", "Hello Go"))
 }
